83755658a645dd92d177eabde219174d9ead7db7
Mohsen ben hassine <mbh851@yahoo.fr>
2026-09-23 19:36:15 +0100

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+LS-Vine: Learning Vine-Compatible Latent Representations
+for Multivariate Dependence Modeling.
+
+Source package for the LS-Vine framework.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Mohsen Ben Hassine, Lamine Mili"
